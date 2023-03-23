7f0d90b76b13396c45be76174c08171b31e8dbeb
JellyYJ <lyj10000h@gmail.com>
2023-03-23 18:25:23 +0000

    resolve create.ejs conflicts

--- routes/create.js
+++ routes/create.js
@@ -1,12 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+
 const sighting_controller = require("../controllers/sighting");
 
-
-
-router.post('/create', sighting_controller.insertSighting);
-
-
-
+router.post("/create", sighting_controller.insertSighting);
 
 module.exports = router;
