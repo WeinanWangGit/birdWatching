c20dfb9be1a485d97af1bbc28e867f32ed52dc44
JellyYJ <lyj10000h@gmail.com>
2023-04-13 21:53:48 +0100

    Get data from database, Image is not working yet

--- models/sighting.js
+++ models/sighting.js
@@ -1,38 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+// Define a Mongoose schema for the bird sighting data
 const SightingSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  identification: {
+    birdName: {
+      type: String,
+      required: true,
+    },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    date: {
-        type: Date,
-        required: true
+    url: {
+      type: String,
     },
-    author: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    identification: {
-        birdName: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String,
-            required: true
-        },
-        url: {
-            type: String
-        }
-    },
-    image: {
-        type: Buffer
-    }
+  },
+  image: {
+    type: Buffer,
+  },
 });
 
-module.exports = mongoose.model('Sighting', SightingSchema);
+module.exports = mongoose.model("Sighting", SightingSchema);
