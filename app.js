24f506f0265745a8ce9598d01690b8c656f0b4df
WeinanWanggit <114651132+WeinanWanggit@users.noreply.github.com>
2023-04-18 17:44:12 +0100

    modify images display

--- app.js
+++ app.js
@@ -28,13 +28,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', createRouter);
 app.use('/', detailsRouter);
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
