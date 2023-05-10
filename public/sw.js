54402a71f06557d11be2fb35e565f0bff597696c
JellyYJ <lyj10000h@gmail.com>
2023-05-10 23:37:10 +0100

    Update index_offline.html

--- public/sw.js
+++ public/sw.js
@@ -1,86 +1,84 @@
 const addResourcesToCache = async (resources) => {
-    const cache = await caches.open("v1");
-    await cache.addAll(resources);
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
 };
 
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        addResourcesToCache([
-            "/",
-            "/public/javascripts/create.js",
-            "/public/javascripts/idb.js",
-            "/page/create_offline.html",
-            "/page/details_offline.html",
-            "/page/index_offline.html",
-            "/stylesheets/create_offline.css",
-            "/stylesheets/details_offline.css",
-            // "/public/uploads/*"
-        ]),
-    );
+  event.waitUntil(
+    addResourcesToCache([
+      "/",
+      "/public/javascripts/create.js",
+      "/public/javascripts/idb.js",
+      "/page/create_offline.html",
+      "/page/details_offline.html",
+      "/page/index_offline.html",
+      "/stylesheets/create_offline.css",
+      "/stylesheets/details_offline.css",
+      "/stylesheets/index_offline.css",
+      // "/public/uploads/*"
+    ])
+  );
 });
 
 const putInCache = async (request, response) => {
-    const cache = await caches.open("v1");
-    await cache.put(request, response);
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
 };
 
-const networkFirst = async ({ request, fallbackUrl}) => {
+const networkFirst = async ({ request, fallbackUrl }) => {
+  //  First try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request);
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    // putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    console.log("get from cache");
+    console.log(request);
 
-    //  First try to get the resource from the network
-    try {
-        const responseFromNetwork = await fetch(request);
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        // putInCache(request, responseFromNetwork.clone());
-        return responseFromNetwork;
-    } catch (error) {
-
-        console.log('get from cache')
-        console.log(request)
-
-        // Next try to get the resource from the cache
-        const responseFromCache = await caches.match(request);
-        if (responseFromCache) {
-            return responseFromCache;
-        }
-
-        //todo
-        // const fallbackResponse = await caches.match(fallbackUrl);
-        // if (fallbackResponse) {
-        //     return fallbackResponse;
-        // }
-
-        // when even the fallback response is not available,
-        // there is nothing we can do, but we must always
-        // return a Response object
-        return new Response("Network error happened", {
-            status: 408,
-            headers: { "Content-Type": "text/plain" },
-        });
+    // Next try to get the resource from the cache
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      return responseFromCache;
     }
 
+    //todo
+    // const fallbackResponse = await caches.match(fallbackUrl);
+    // if (fallbackResponse) {
+    //     return fallbackResponse;
+    // }
+
+    // when even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object
+    return new Response("Network error happened", {
+      status: 408,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
 };
 
 self.addEventListener("fetch", (event) => {
-    // If url is from external then just skip it
-    if (!event.request.url.startsWith(event.currentTarget.origin)) {
-        return
-    }
+  // If url is from external then just skip it
+  if (!event.request.url.startsWith(event.currentTarget.origin)) {
+    return;
+  }
 
-    //Ignore any of the API requests as these shouldn't be cached. instead these are stored in the idb
-    let ignoreUrls = ['/socket.io/?']
+  //Ignore any of the API requests as these shouldn't be cached. instead these are stored in the idb
+  let ignoreUrls = ["/socket.io/?"];
 
-    if (!(ignoreUrls.some(url => event.request.url.startsWith(event.currentTarget.origin + url)))) {
-        event.respondWith(
-            networkFirst({
-                request: event.request,
-                fallbackUrl: "/gallery/myLittleVader.jpg",
-            })
-        );
-    }
-
+  if (
+    !ignoreUrls.some((url) =>
+      event.request.url.startsWith(event.currentTarget.origin + url)
+    )
+  ) {
+    event.respondWith(
+      networkFirst({
+        request: event.request,
+        fallbackUrl: "/gallery/myLittleVader.jpg",
+      })
+    );
+  }
 });
-
-
-
