e9e095af37754d150e0ec272ca4e82b441fb4d58
JellyYJ <lyj10000h@gmail.com>
2023-05-13 16:01:49 +0100

    Disable some buttons when offline

---
+++ public/javascripts/disableButtons.js
@@ -0,0 +1,22 @@
+// disable buttons when offline
+function disableButtons() {
+  // disable all buttons on the page except for the button in the header and close button
+  const buttons = document.querySelectorAll(
+    "button:not(.header-button):not(.close-button)"
+  );
+  buttons.forEach((button) => (button.disabled = true));
+}
+
+function enableButtons() {
+  // enable all buttons on the page
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => (button.disabled = false));
+}
+
+window.addEventListener("offline", () => {
+  disableButtons();
+});
+
+window.addEventListener("online", () => {
+  enableButtons();
+});
